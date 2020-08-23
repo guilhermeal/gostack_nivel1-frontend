2e08124dbe96b61563aff00ac054865aef51566c
Guilherme Almeida <guiallan.al@gmail.com>
2020-08-22 22:29:42 -0300

    Conceitos de Componentes e Propriedades
    AUla #0012 e #0013
    
    Ref: NIVEL1

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Painel</li>
+          <li>Login</li>
+        </ul>
+      </Header>
+    </>
+
+  );
+}
+
+export default App;
