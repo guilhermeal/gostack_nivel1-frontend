a837cff1610dbf397cb29b0108411fc66f3c9a79
Guilherme Almeida <guiallan.al@gmail.com>
2020-08-23 00:01:33 -0300

    Cria e configura Webpack com Loaders de css e imagens
    Aula #0016
    
    REF: NIVEL1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import './App.css';
+import backgroundImage from './assets/background.jpg';
+
 import Header from './components/Header'
 
 function App() {
@@ -20,6 +23,8 @@ function App() {
   return (
     <>
       <Header title="Projects" />
+
+      <img width={400} src={backgroundImage} alt="Background"/>
         
       <ul>
         {projects.map(project => <li key={project}>{project}</li> )}
